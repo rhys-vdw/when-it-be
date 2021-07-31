0c3818541ab8767aa2d34aaf39e2c263abc5eac4
Rhys van der Waerden <rhys.vdw@gmail.com>
2021-07-31 15:28:51 +1000

    Temporarily change release branch

--- gh-pages.sh
+++ gh-pages.sh
@@ -2,9 +2,9 @@
 
 git checkout gh-pages
 git pull origin gh-pages --no-edit
-git merge master --no-edit
+git merge preact --no-edit
 npm run build
 git add -f bundle.js
 git commit -m "Update gh-pages"
 git push -f origin gh-pages
-git checkout master
+git checkout preact
